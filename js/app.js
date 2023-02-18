20b20069463f2be014293fc09d39c1203fe858a4
Jeremy <jrob1time@gmail.com>
2023-02-17 18:35:41 -0600

    update winningcombos

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const messageEl = document.getElementById("message");
 const squareEls = document.querySelector("#.sqr");
 const resetBtnEl = document.querySelector("#reset");
 let turn, winner, tie, board;
+// squareEls?.addEventListener('click', handleBtnClick)
+// resetBtnEl?.addEventListener('click', handleBtnClick)
 function init() {
     board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     turn = 1;
--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,10 @@ type winningCombos = [[0, 1, 2],[3, 4, 5],[6, 7, 8],
 
 let turn: number, winner: boolean, tie: boolean, board: number[]
 
+// squareEls?.addEventListener('click', handleBtnClick)
+
+
+// resetBtnEl?.addEventListener('click', handleBtnClick)
 
 
 function init() {

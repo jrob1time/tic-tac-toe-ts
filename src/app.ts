d8f04bad03d21f7f94e6a426782acd427a793b40
Jeremy <jrob1time@gmail.com>
2023-02-18 17:18:26 -0600

    update board

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,14 @@
-// const messageEl = document.getElementById("message")
-
-// const squareEls = document.querySelector<HTMLElement>("#.sqr")!
-
-// const resetBtnEl = document.querySelector<HTMLButtonElement>("#reset")!
-
-
-// type winningCombos = [[0, 1, 2],[3, 4, 5],[6, 7, 8],
-// [0, 3, 6],[1, 4, 7],[2, 5, 8],
-// [0, 4, 8],[2, 4, 6]]
-
-
-// type Turn = "x" | "o" | ""
-// type Winner = false
-// type Tie = false
-
-// let turn: string, winner: boolean, tie: boolean, board: number[]
-
-
-
-// squareEls?.addEventListener('click', handleBtnClick)
-
-
-// resetBtnEl?.addEventListener('click', handleBtnClick)
-
-
-// function init() {
-//   board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-//   turn = "x"
-//   winner = false
-//   tie = false
-// }
-
-
 /*-------------------------------- Constants --------------------------------*/
 
 const squareEls: NodeListOf<Element> = document.querySelectorAll(".sqr");
+
+
 const messageEl: HTMLElement | null = document.getElementById("message");
+
+
 const resetBtnEl: HTMLButtonElement | null = document.querySelector(".reset-button");
+
+
 const winningCombos: number[][] = [
   [0, 1, 2],[3, 4, 5],[6, 7, 8],
   [0, 3, 6],[1, 4, 7],[2, 5, 8],
@@ -68,10 +40,6 @@ function init(): void {
   render();
 }
 
-function render(): void {
-  updateBoard();
-  updateMessage();
-}
 
 function updateBoard(): void {
   board.forEach((square: number | null, idx: number) => {
@@ -86,46 +54,4 @@ function updateBoard(): void {
       return;
     }
   });
-}
-
-function updateMessage(): void {
-  if (winner === false && tie === false) {
-    messageEl!.innerText = `It is player ${turn === 1 ? 'X' : 'O'}'s turn`;
-  } else if (winner === false && tie === true) {
-    messageEl!.innerText = "It's a tie!";
-  } else {
-    messageEl!.innerText = `Player ${turn === 1 ? 'X' : 'O'} Win's!`;
-  }
-}
-
-function handleClick(evt: MouseEvent): void {
-  const sqrIdx: number = parseInt((evt.target as HTMLElement).id.slice(2));
-  if (board[sqrIdx] !== null) return;
-  if (winner === true) return;
-  placePiece(sqrIdx);
-  checkForTie();
-  checkForWinner();
-  switchPlayerTurn();
-  render();
-}
-
-function placePiece(idx: number): void {
-  board[idx] = turn;
-}
-
-function checkForTie(): void {
-  if (board.includes(null)) return;
-  tie = true;
-}
-
-function checkForWinner(): void {
-  winningCombos.forEach((combo: number[]) => {
-    if (Math.abs(board[combo[0]]! + board[combo[1]]! + board[combo[2]]!) === 3) {
-      winner = true;
-    }
-  });
-}
-
-function switchPlayerTurn(): void {
-  if (!winner) turn *= -1;
 }
